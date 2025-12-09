3dfb39e6c23088bc7ff724c121ed31bbae60f232
Koutsura <m.denny.trilisandi@gmail.com>
2025-12-09 14:21:11 +0700

    update agar buka web pertama langsung ke index

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TesController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/tes');
 });
 
 Route::get('/tes', [TesController::class, 'index']);
