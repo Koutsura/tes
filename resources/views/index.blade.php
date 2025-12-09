8998f9cfd7cb17f1f7121ee02b965cebbb8bfcf3
Koutsura <m.denny.trilisandi@gmail.com>
2025-12-09 11:02:35 +0700

    update tampilan index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,19 +5,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Beranda</title>
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        table, th, td {
+            border: 1px solid black;
+            padding: 8px;
+            text-align: left;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
 </head>
 <body>
     <a href="/tes/create" class="btn btn-primary">tambah data</a>
-    <div class ="form container">
-          <table class="table table-bordered">
-            <thead>
+    <div class="table-responsive">
+        <table class="table table-bordered table-striped table-hover align-middle">
+            <thead class="table-primary text-center">
                 <tr>
-                    <th>Nama</th>
-                    <th>tanggal lahir</th>
+                    <th scope="col">Nama</th>
+                    <th scope="col">tanggal lahir</th>
                 </tr>
             </thead>
-          </table>
-
+            <tbody>
+                @foreach ($pasien as $item)
+                    <tr>
+                        <td>{{ $item->nama }}</td>
+                        <td>{{ $item->tgl_lahir }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 </body>
 </html>
